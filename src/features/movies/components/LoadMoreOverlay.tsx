1bb1c3b85768586ee64511f23649113e2cba8a12
miftawidaya <miftawidaya@gmail.com>
2026-01-25 23:39:00 +0700

    feat: Implement dynamic overlay height and refine card grid width calculations using new CSS variables.

--- src/features/movies/components/LoadMoreOverlay.tsx
+++ src/features/movies/components/LoadMoreOverlay.tsx
@@ -13,7 +13,7 @@ export const LoadMoreOverlay: FC<Readonly<LoadMoreOverlayProps>> = ({
 }) => {
   return (
     <div className='absolute bottom-0 left-1/2 z-30 flex w-screen -translate-x-1/2 flex-col justify-end'>
-      <div className='from-base-black via-base-black/60 relative flex h-96.5 w-full flex-col items-center justify-end bg-linear-to-t from-25% via-65% to-transparent pb-18.5 md:h-124 md:justify-center md:pb-0'>
+      <div className='from-base-black to-base-black/60 h-overlay-height relative flex w-full flex-col items-center justify-end bg-linear-to-t from-10% pb-22.5 md:justify-center md:from-20% md:pb-0'>
         <Button
           variant='secondary'
           size='lg'
